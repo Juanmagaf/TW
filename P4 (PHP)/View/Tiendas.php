1f282655ec4e4d0f40c044598d65b85c50e43af4
Juan Manuel González-Aurioles <32701149+Juanmagaf@users.noreply.github.com>
2019-12-30 19:23:42 +0100

    Create Tiendas.php

---
+++ P4 (PHP)/View/Tiendas.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Juan Manuel González-Aurioles">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="css/Css.css">
+    <title>Página Tiendas Libreria</title>
+</head>
+<body>
+        <!-- Contenido principal -->
+    <main>
+        <!-- Tabla -->
+    <h2>Tiendas</h2>   
+    <table>
+    <!-- Primera Fila -->
+    <tr>
+     <th>Granada</th>
+     <th>Málaga</th>
+     </tr>
+            <!-- Segunda Fila -->
+     <tr>
+         <td><img src="Imagenes/Picasso.PNG" alt="Lib_Picasso"/>
+                <p><strong>Libreria Picasso</strong></p>
+         <p>Calle Obispo Hurtado, 5, 18002 Granada</p>    
+         <p>958 53 69 10</p> 
+         </td>
+
+         <td><img src="Imagenes/lib_luces.PNG" alt="Lib_Luces"/>
+                <p><strong>Libreria Luces</strong></p>
+         <p>Alameda Principal, 37, Calle Trinidad Grund, 30, 29001 Málaga</p>    
+         <p>952 12 21 00</p> 
+         </td>     
+    </tr>
+
+        <!-- Tercera Fila -->
+    <tr>
+        <td><img src="Imagenes/Agapea.jpg" alt="Lib_Agapea"/>
+                <p><strong>Libreria Agapea</strong></p>
+         <p>Calle Puentezuelas 28, 18002 Granada</p>    
+         <p>958 26 71 68</p> 
+         </td> 
+
+        <td><img src="Imagenes/proteo.PNG" alt="Lib_Proteo"/>
+        <p><strong>Libreria Proteo Prometeo</strong></p>
+        <p>Calle Puerta Buenaventura, 3, 29008 Málaga</p>    
+        <p>952 21 90 19</p> 
+        </td>
+
+     </tr>
+    </table>
+        
+        
+</main>
+    
+    
+    <?php
+       // require("Lateral.php");
+    ?>
+</body>
+</html>
