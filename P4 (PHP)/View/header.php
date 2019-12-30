8b652676f2e602dac39eb420e6c1424ca1430a1c
Juan Manuel González-Aurioles <32701149+Juanmagaf@users.noreply.github.com>
2019-12-30 19:24:04 +0100

    Create header.php

---
+++ P4 (PHP)/View/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="author" content="Juan Manuel González-Aurioles">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="css/Css.css">
+
+    <title>Página Catalogo Libreria</title>
+</head>
+
+    <header>
+        <img src="Imagenes/libro.jpg" alt="Logo"/>
+        <h1 id ="indice">El Lector de Libros</h1>
+        <p>Contenido historias desde hace 100 años</p>
+    </header>
+<?php
+    include("nav.php")
+?>
+    
+</html>
